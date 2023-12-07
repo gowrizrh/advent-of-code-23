70bd10d5b5177af6c10a277313d25eb993802799
Gowri <gowri.sankar@aligent.com.au>
2023-12-08 01:52:01 +1030

    :tada: day 7 part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -18,10 +18,10 @@ val power = mapOf(
 fun main() {
     val input = readInput("Day07")
 
+    // Part 1
     input
-        .map { game ->
-            game.split(" ")
-        }
+        .asSequence()
+        .map { it.split(" ") }
         .map { game ->
             val cards = game.first().map { it }
             val bid = game.last().toInt()
@@ -33,18 +33,55 @@ fun main() {
                 .groupingBy { it.first }
                 .eachCount()
                 .toSortedMap()
-                .let { type ->
-                    when (type) {
-                        mapOf(1 to 5) -> Type.HIGHCARD
-                        mapOf(1 to 3, 2 to 1) -> Type.ONEPAIR
-                        mapOf(1 to 1, 2 to 2) -> Type.TWOPAIR
-                        mapOf(1 to 2, 3 to 1) -> Type.THREEOFAKIND
-                        mapOf(2 to 1, 3 to 1) -> Type.FULLHOUSE
-                        mapOf(1 to 1, 4 to 1) -> Type.FOUROFAKIND
-                        mapOf(5 to 1) -> Type.FIVEOFAKIND
-                        else -> null
+                .let { fold(it) }
+
+            Hand(cards, bid, type!!)
+        }
+        .sorted()
+        .mapIndexed { index, hand ->
+            hand.bid * (index + 1)
+        }
+        .sum()
+        .also(::println)
+
+    // Part 2
+    input
+        .asSequence()
+        .map { game ->
+            game.split(" ")
+        }
+        .map { game ->
+            val cards = game.first().map { it }
+            val bid = game.last().toInt()
+
+            val type = game.first()
+                .groupingBy { it }
+                .eachCount()
+                .let {
+                    val newMap = it.toMutableMap()
+
+                    if (newMap.contains('J') && newMap['J'] != 5) { // when everything is J then it's five of a kind
+                        val bestMatch = it.maxBy { entry ->
+                            if (entry.key != 'J') {
+                                entry.value
+                            } else {
+                                0
+                            }
+                        }.key
+
+//                        println("$bestMatch: ${newMap[bestMatch]!! + newMap['J']!!}")
+
+                        newMap[bestMatch] = it[bestMatch]!! + it['J']!!
+                        newMap.remove('J')
                     }
+
+                    newMap
                 }
+                .map { it.value to it.key }
+                .groupingBy { it.first }
+                .eachCount()
+                .toSortedMap()
+                .let { fold(it) }
 
             Hand(cards, bid, type!!)
         }
@@ -57,6 +94,18 @@ fun main() {
 
 }
 
+fun fold(type: Map<Int, Int>): Type? {
+    return when (type) {
+        mapOf(1 to 5) -> Type.HIGHCARD
+        mapOf(1 to 3, 2 to 1) -> Type.ONEPAIR
+        mapOf(1 to 1, 2 to 2) -> Type.TWOPAIR
+        mapOf(1 to 2, 3 to 1) -> Type.THREEOFAKIND
+        mapOf(2 to 1, 3 to 1) -> Type.FULLHOUSE
+        mapOf(1 to 1, 4 to 1) -> Type.FOUROFAKIND
+        mapOf(5 to 1) -> Type.FIVEOFAKIND
+        else -> null
+    }
+}
 
 data class Hand(val cards: List<Char>, val bid: Int, val type: Type) : Comparable<Hand> {
     override fun compareTo(other: Hand): Int {
@@ -80,11 +129,11 @@ data class Hand(val cards: List<Char>, val bid: Int, val type: Type) : Comparabl
 }
 
 enum class Type(val strength: Int) {
-    HIGHCARD(1),
-    ONEPAIR(2),
-    TWOPAIR(3),
-    THREEOFAKIND(4),
-    FULLHOUSE(5),
-    FOUROFAKIND(6),
     FIVEOFAKIND(7),
+    FOUROFAKIND(6),
+    FULLHOUSE(5),
+    THREEOFAKIND(4),
+    TWOPAIR(3),
+    ONEPAIR(2),
+    HIGHCARD(1),
 }
