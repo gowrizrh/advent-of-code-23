59dc1b6e8f4a270d49aab513c6fe1696a952c0d0
Gowri <gowri.sankar@aligent.com.au>
2023-12-12 17:57:52 +1030

    :sparkles: day 11 part 2

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,34 +1,35 @@
 import kotlin.math.abs
 
 fun main() {
-    println("\"--- Day 11: Cosmic Expansion ---\"")
+    println("--- Day 11: Cosmic Expansion ---")
     val input = readInput("Day11")
 
-    val universe = Image(input.toMutableList())
-    val galaxies = universe.galaxies()
-    println("Galaxy pairs found: ${galaxies.size}")
+    val universe = Instrument(input)
+    println("Galaxy pairs found: ${universe.galaxies.size}")
 
-    galaxies
-        .sumOf { manhattan(it.first, it.second) }
-        .also { println(it) }
+    // Part 1
+    universe.expansion = 1
+    universe
+        .galaxies
+        .sumOf { (start, end) -> universe.manhattan(start, end) }
+        .also(::println)
+
+    // Part 2
+    universe.expansion = 999999
+    universe
+        .galaxies
+        .sumOf { (start, end) -> universe.manhattan(start, end) }
+        .also(::println)
 
     return
 }
 
-fun manhattan(start: Space, end: Space): Int {
-    val dx: Int = abs(start.x - end.x)
-    val dy: Int = abs(start.y - end.y)
-
-    return dx + dy
-}
-
-class Image(input: MutableList<String>) {
-    private val rows = mutableListOf<MutableList<Space>>()
+class Instrument(input: List<String>, var expansion: Long = 1) {
+    private val cosmicRows = mutableSetOf<Int>()
+    private val cosmicColumns = mutableSetOf<Int>()
+    var galaxies = mutableListOf<Pair<Pair<Int, Int>, Pair<Int, Int>>>()
 
     init {
-        val cosmicRows = mutableSetOf<Int>()
-        val cosmicColumns = mutableSetOf<Int>()
-
         for ((x, line) in input.withIndex()) {
             if (line.all { it == '.' }) {
                 cosmicRows.add(x)
@@ -46,94 +47,36 @@ class Image(input: MutableList<String>) {
             }
         }
 
-        for ((rowOffset, row) in cosmicRows.withIndex()) {
-            input.add(row + rowOffset, String(input[row + rowOffset].toCharArray()))
-        }
-
-        for ((index, line) in input.withIndex()) {
-            val stringBuilder = StringBuilder(line)
-
-            for ((columnsOffset, column) in cosmicColumns.withIndex()) {
-
-                stringBuilder.insert(column + columnsOffset, '.')
-            }
-
-            input[index] = stringBuilder.toString()
-        }
+        val galaxy = mutableListOf<Pair<Int, Int>>()
 
         for ((x, line) in input.withIndex()) {
-            val column = mutableListOf<Space>()
-
-            for ((y, type) in line.withIndex()) {
-                column.add(Space(x, y, type))
-            }
-
-            rows.add(column)
-        }
-
-        println("Rows expanded: $cosmicRows")
-        println("Columns expanded: $cosmicColumns")
-    }
-
-    fun galaxies(): List<Pair<Space, Space>> {
-        val pairs = mutableListOf<Pair<Space, Space>>()
-        val galaxy = mutableListOf<Space>()
-
-        for (x in 0..<rows.size) {
-            for (y in 0..<rows[x].size) {
-                if (rows[x][y].type == '#') {
-                    galaxy.add(rows[x][y])
+            for ((y, token) in line.withIndex()) {
+                if (token == '#') {
+                    galaxy.add(Pair(x, y))
                 }
             }
         }
 
         for (i in 0..<galaxy.size) {
             for (j in i + 1..<galaxy.size) {
-                pairs.add(Pair(galaxy[i], galaxy[j]))
+                galaxies.add(Pair(galaxy[i], galaxy[j]))
             }
         }
 
-        return pairs
-    }
-}
-
-data class Space(
-    var x: Int,
-    var y: Int,
-    val type: Char,
-    var f: Int = Int.MAX_VALUE,
-    var g: Int = Int.MAX_VALUE,
-    var parent: Space? = null
-) : Comparable<Space> {
-    override fun hashCode(): Int {
-        val prime = 31
-        var result = 1
-        result = prime * result + y
-        result = prime * result + x
-        return result
+        println("Rows expanded: $cosmicRows")
+        println("Columns expanded: $cosmicColumns")
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+    fun manhattan(start: Pair<Int, Int>, end: Pair<Int, Int>): Long {
+        val pairOneXOffset = cosmicRows.filter { it < start.first }.size * expansion
+        val pairOneYOffset = cosmicColumns.filter { it < start.second }.size * expansion
 
-        other as Space
+        val pairTwoXOffset = cosmicRows.filter { it < end.first }.size * expansion
+        val pairTwoYOffset = cosmicColumns.filter { it < end.second }.size * expansion
 
-        if (x != other.x) return false
-        if (y != other.y) return false
-        if (type != other.type) return false
+        val dx = abs((start.first + pairOneXOffset) - (end.first + pairTwoXOffset))
+        val dy = abs((start.second + pairOneYOffset) - (end.second + pairTwoYOffset))
 
-        return true
-    }
-
-    override operator fun compareTo(other: Space): Int {
-        if (f > other.f) return 1 else if (f < other.f) {
-            return -1
-        }
-        return 0
-    }
-
-    override fun toString(): String {
-        return "($x,$y)"
+        return dx + dy
     }
 }
